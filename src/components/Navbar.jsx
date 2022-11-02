3f07bb46959249d35d474d2af2196b3f30640cf2
johncoh98 <John.coh98@gmail.com>
2022-11-02 17:28:42 +0200

    ff

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,14 @@
 import{ Stack} from '@mui/material';
 import { Link } from 'react-router-dom';
 import SearchBar from './SearchBar';
+import ytblue from './ytblue.png';
 
 
 
 const Navbar = () => (
     <Stack direction="row" alignItems="center" p={2} sx={{positon: 'sticky', background: '#000', top: 0, justifyContent: 'space-between'}} >
       <Link to="/" style={{ display: 'flex', alignItems: 'center' }}>
-      <img src="./blueyt.png" alt="Logo Youtube Bleu Png@nicepng.com" height={40}/>
+      <img src={ytblue} alt="Logo" height={40}/>
       </Link>
       <SearchBar />
     </Stack>
